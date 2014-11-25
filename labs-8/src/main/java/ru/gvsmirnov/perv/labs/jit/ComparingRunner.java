0e087b72142383ae631c8af3105a54e70c453090
Gleb Smirnov <me@gvsmirnov.ru>
2014-11-26 02:13:37 +0300

    "Improved" the formatting of comparison results

--- labs-8/src/main/java/ru/gvsmirnov/perv/labs/jit/ComparingRunner.java
+++ labs-8/src/main/java/ru/gvsmirnov/perv/labs/jit/ComparingRunner.java
@@ -1,11 +1,15 @@
 package ru.gvsmirnov.perv.labs.jit;
 
 import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.results.format.ResultFormat;
+import org.openjdk.jmh.results.format.ResultFormatFactory;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.ChainedOptionsBuilder;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.io.PrintWriter;
 import java.util.Collection;
 
 public class ComparingRunner {
@@ -29,15 +33,47 @@ public class ComparingRunner {
     }
 
     private static void printResult(String label, Collection<RunResult> runResults) {
-        runResults.stream()
-                .map(r -> format(label, r))
-                .forEach(System.out::println);
+
+        // Too lazy to write my own formatter
+        // Will use that of JMH
+        // Also will pervert for that
+        // Don't you judge me!
+
+        PrintWriter writer = makeWriter(label);
+        ResultFormat resultFormat = ResultFormatFactory.getInstance(ResultFormatType.TEXT, writer);
+
+        resultFormat.writeOut(runResults);
+        writer.flush();
     }
 
-    private static String format(String label, RunResult result) {
-        String[] parts = result.getParams().getBenchmark().split("\\.");
-        String benchmark = parts[parts.length - 1];
+    private static PrintWriter makeWriter(final String label) {
+        return new PrintWriter(System.out) {
 
-        return String.format("[%s] %s: %s", label, benchmark, result.getPrimaryResult());
+            private StringBuilder builder = makeBuilder();
+
+            @Override
+            public void print(String s) {
+                builder.append(s);
+            }
+
+            @Override
+            public void println() {
+                this.println("");
+            }
+
+            @Override
+            public void println(String s) {
+                builder.append(s);
+                super.print(builder.toString());
+                super.println();
+                this.builder = makeBuilder();
+            }
+
+            private StringBuilder makeBuilder() {
+                return new StringBuilder("[" + label + "]\t");
+            }
+
+        };
     }
+
 }
