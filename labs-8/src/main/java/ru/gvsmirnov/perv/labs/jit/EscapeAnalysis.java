1ddaeb489667d26a7033ab5e9f14580a3372ff9b
Gleb Smirnov <me@gvsmirnov.ru>
2014-11-26 01:29:31 +0300

    Added a basic escape analysis benchmark

---
+++ labs-8/src/main/java/ru/gvsmirnov/perv/labs/jit/EscapeAnalysis.java
@@ -0,0 +1,46 @@
+package ru.gvsmirnov.perv.labs.jit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.RunnerException;
+
+import static ru.gvsmirnov.perv.labs.jit.ComparingRunner.runBenchmarks;
+
+@State(Scope.Benchmark)
+public class EscapeAnalysis {
+    public int id = (int) (Math.random() * Integer.MAX_VALUE);
+
+    public static class Convict {
+        public final int id;
+
+        public Convict(int id) {
+            this.id = id;
+        }
+    }
+
+    @Benchmark
+    public Object baseline_returnObject() {
+        return this;
+    }
+
+    @Benchmark
+    public int baseline_returnInt() {
+        return this.id;
+    }
+
+    @Benchmark
+    public Object measureEscape() {
+        return new Convict(id);
+    }
+
+    @Benchmark
+    public int measureNoEscape() {
+        return new Convict(id).id;
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        runBenchmarks(EscapeAnalysis.class);
+    }
+
+}
