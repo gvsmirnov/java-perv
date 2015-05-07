65573f1cf91853b4e4cbae4ec4c97f1756cce22c
Gleb Smirnov <me@gvsmirnov.ru>
2015-05-07 12:44:46 +0300

    Added an example of how many weak refs may lead to more frequent full GC pauses

---
+++ labs-8/src/main/java/ru/gvsmirnov/perv/labs/gc/WeakReferences.java
@@ -0,0 +1,61 @@
+package ru.gvsmirnov.perv.labs.gc;
+
+import java.lang.ref.WeakReference;
+import java.util.Queue;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class WeakReferences {
+
+//    private static final boolean DO_WEAK_REFERENCES  = Boolean.getBoolean("weak.refs");
+//    private static final boolean DO_WEAK_REFERENCES  = false;
+    private static final boolean DO_WEAK_REFERENCES  = true;
+
+    // Run with: -verbose:gc -Xmx64m -XX:MaxTenuringThreshold=0
+    // Observe that there are only young GCs
+
+    // Run with: -verbose:gc -Xmx64m -XX:MaxTenuringThreshold=0 -Dweak.refs=true
+    // Observe that there are many full GCs, even though the allocation pattern is the same
+
+    private static Object makeObject() {
+        return new byte[64 * 1024];
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReferenceCleaner.setup();
+
+        while(true) {
+            createWeakReference(makeObject());
+        }
+    }
+
+    private static void createWeakReference(Object object) {
+        if(!DO_WEAK_REFERENCES) {
+            object = SUBSTITUTE_OBJECT;
+        }
+
+        weakRefs.offer(new WeakReference<>(object));
+    }
+
+    private static final Object SUBSTITUTE_OBJECT = makeObject();
+    private static final Queue<WeakReference<Object>> weakRefs = new ArrayBlockingQueue<>(1 << 16);
+
+    private static class ReferenceCleaner extends TimerTask {
+
+        private static final Timer timer = new Timer(true);
+
+        @Override
+        public void run() {
+            int size = weakRefs.size();
+            for(int i = 0; i < size; i ++) {
+                weakRefs.poll();
+            }
+            System.out.println("Cleaned out");
+        }
+
+        private static void setup() {
+            timer.scheduleAtFixedRate(new ReferenceCleaner(), 0, 1000);
+        }
+    }
+}
