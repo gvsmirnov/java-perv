1b18fadb0b321e049dfe59680e93042f425165f8
Gleb Smirnov <me@gvsmirnov.ru>
2015-08-30 23:34:09 +0300

    Formatting

--- labs-8/src/main/java/ru/gvsmirnov/perv/labs/rekt/ChecksumCalculator.java
+++ labs-8/src/main/java/ru/gvsmirnov/perv/labs/rekt/ChecksumCalculator.java
@@ -10,10 +10,9 @@ public class ChecksumCalculator {
 
         System.loadLibrary("main");
 
-        Stream.of(args)
-                .forEach(filename -> {
-                    long checksum = calculateChecksum(filename);
-                    System.out.println(filename + '\t' + checksum);
-                });
+        Stream.of(args).forEach(filename -> {
+            long checksum = calculateChecksum(filename);
+            System.out.println(filename + '\t' + checksum);
+        });
     }
 }
