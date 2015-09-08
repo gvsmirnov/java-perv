7805de48c150e35fd2561ca0107a22e0fd8668b9
Gleb Smirnov <me@gvsmirnov.ru>
2015-09-08 12:33:27 +0300

    Made the C file cleaner and commented

--- labs-8/src/main/c/ChecksumCalculator.c
+++ labs-8/src/main/c/ChecksumCalculator.c
@@ -6,17 +6,10 @@
 #endif
 
 int digest(char *src_filename, char *dst_filename) {
-  int checksum = rand(); // TODO: calculate the actual checksum
-
-  FILE* digested = fopen(dst_filename, "w");
-
-  if(digested != NULL) {
-    fprintf(digested, "%d\n", checksum);
-    fclose(digested);
-    return checksum;
-  } else {
-    return -1;
-  }
+  // For this demonstration, we do not actually need to do anything here
+  // Assume that this function opens src file, calculates the checksum
+  // and writes it to dst_filename.
+  return rand();
 }
 
 JNIEXPORT jlong JNICALL Java_ru_gvsmirnov_perv_labs_rekt_ChecksumCalculator_calculateChecksum
@@ -26,12 +19,16 @@ JNIEXPORT jlong JNICALL Java_ru_gvsmirnov_perv_labs_rekt_ChecksumCalculator_calc
     const char *src_filename;
     char        dst_filename[MAX_FILE_NAME_LENGTH];
 
+    // Get the source file name
     src_filename = (*jniEnv)->GetStringUTFChars(jniEnv, filename, NULL);
 
+    // Get the name of the file to write checksum to (and "accidentally" overflow the buffer)
     sprintf(dst_filename, "%s.digested", src_filename);
 
+    // Write the checksum to the destination file and also get its value
     result = digest(src_filename, dst_filename);
 
+    // Free the memory used for source file name
     (*jniEnv)->ReleaseStringUTFChars(jniEnv, filename, src_filename);
 
     printf("Exiting native method with checksum: %d\n", result);
