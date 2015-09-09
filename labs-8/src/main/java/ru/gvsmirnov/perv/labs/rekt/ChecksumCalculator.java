4824e6589bcc0e69bf6f2509fab93c50b524d108
Gleb Smirnov <me@gvsmirnov.ru>
2015-09-09 13:39:06 +0200

    Simplified java code

--- labs-8/src/main/java/ru/gvsmirnov/perv/labs/rekt/ChecksumCalculator.java
+++ labs-8/src/main/java/ru/gvsmirnov/perv/labs/rekt/ChecksumCalculator.java
@@ -1,7 +1,5 @@
 package ru.gvsmirnov.perv.labs.rekt;
 
-import java.util.stream.Stream;
-
 public class ChecksumCalculator {
 
     private static native long calculateChecksum(String filename);
@@ -9,21 +7,7 @@ public class ChecksumCalculator {
     public static void main(String[] args) {
 
         System.loadLibrary("main");
-
-        Thread calculator = new Thread(() -> {
-            Stream.of(args).forEach(filename -> {
-                long checksum = calculateChecksum(filename);
-                System.out.println("Got checksum from native method: " + checksum);
-            });
-        });
-
-        calculator.start();
-        try {
-            calculator.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Finished");
+        long checksum = calculateChecksum(args[0]);
+        System.out.println("Got checksum from native method: " + checksum);
     }
 }
